8d0c6754757b04af9fcc69bad20efe076ff5b0b3
McQueen <flyingmcqueen69@gamil.com>
2023-09-22 10:25:09 +0530

    Version 3 error

--- config.js
+++ config.js
@@ -1,1 +1,2 @@
-console.log("Hey there! This is version 2");
+console.log("Hey there! This is version 3")
+console.log("There is an error in this file. Try going back and restore the code in version 1")
--- src/user.js
+++ src/user.js
@@ -1,1 +1,1 @@
-console.log("This is user 1 committing version 2")
+console.log("This is user 1 committing version 3")
